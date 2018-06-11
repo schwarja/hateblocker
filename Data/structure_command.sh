84a94d034a14902abcfa2183e110d19bbcb77c1c
Jan Schwarz <jan.schwarz@strv.com>
2018-06-11 10:59:48 +0200

    Update dataset

--- Data/structure_command.sh
+++ Data/structure_command.sh
@@ -1,1 +1,3 @@
 cat labeled_data.csv | grep -e "^[0-9][0-9]*," | sed 's/[0-9]*,[0-9]*,[0-9]*,[0-9]*,[0-9]*,//; /^$/d; s/"*//g; s/\([0-9]*\),\(.*\)/\{^M"text": "\2",^M"label": "\1"^M\},/'
+
+cat labeled_data.csv | grep -e "^[0-9][0-9]*," | sed 's/[0-9]*,[0-9]*,[0-9]*,[0-9]*,[0-9]*,//; /^$/d; s/"*//g; s/\([0-9]*\),\(.*\)/\{^M"text": "\2",^M"label": "\1"^M\},/; s/\\//g; s/"label": "2"/"label": "correct"/; ; s/"label": "1"/"label": "offensive"/; s/"label": "0"/"label": "hateful"/'
