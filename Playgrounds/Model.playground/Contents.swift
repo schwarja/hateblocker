2f11b9b3e4d64690fb43fb01ba4808cb10156bd5
Jan Schwarz <jan.schwarz@strv.com>
2018-06-13 11:27:39 +0200

    Specify ML algorithm

--- Playgrounds/Model.playground/Contents.swift
+++ Playgrounds/Model.playground/Contents.swift
@@ -1,9 +1,12 @@
 import Foundation
 import CreateML
+import NaturalLanguage
 
 let url = Bundle.main.url(forResource: "structured_data", withExtension: "json")!
 let data = try MLDataTable(contentsOf: url)
 
-let model = try MLTextClassifier(trainingData: data, textColumn: "text", labelColumn: "label")
+let algorithm = MLTextClassifier.ModelAlgorithmType.maxEnt(revision: 1)
+let parameters = MLTextClassifier.ModelParameters(validationData: [:], algorithm: algorithm, language: .english)
+let model = try MLTextClassifier(trainingData: data, textColumn: "text", labelColumn: "label", parameters: parameters)
 
-try model.write(to: URL(fileURLWithPath: "/Users/strv/Desktop/HatredModel.mlmodel"))
+try model.write(to: URL(fileURLWithPath: "/Users/strv/Desktop/HatredModel-MaxEnt.mlmodel"))
